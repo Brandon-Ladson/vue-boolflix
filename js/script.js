f59d50e89641b95ae3c7a51e3ccdd7027d022d1a
Brandon Ladson <brandonladson1999@gmail.com>
2020-12-02 16:56:28 +0100

    creazione prime regole js per stampare sulla pagina titolo, titolo originale, lingua e voto dei film cercati

--- js/script.js
+++ js/script.js
@@ -1,7 +1,30 @@
+// https://api.themoviedb.org/3/search/movie?api_key=75fa11ba07d05d73ee35e8943a4a42f9&query='ricercaUtente'
+const apiKey = '75fa11ba07d05d73ee35e8943a4a42f9';
+const urlBase = 'https://api.themoviedb.org/3/search/movie?api_key=';
+const lingua = '&language=it-IT&';
+
 var app = new Vue({
   el: '#app',
   data: {
+    ricercaUtente: '',
+    filmTrovati: []
+  },
+  methods: {
 
+    searchMovie: function () {
+
+      axios.get(urlBase + apiKey + lingua + '&query=' + this.ricercaUtente)
+      .then(risultato => {
+
+        this.filmTrovati = risultato.data.results;
+
+        console.log(this.filmTrovati);
+
+      });
+
+      this.ricercaUtente = '';
+
+    }
   }
 
 
