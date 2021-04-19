a967a3edc2dcbb3fd5994bc1e4253991d3a41321
Brandon Ladson <brandonladson1999@gmail.com>
2021-04-19 21:19:42 +0200

    aggiunta generi dei film e delle serie

--- js/script.js
+++ js/script.js
@@ -3,10 +3,12 @@ var app = new Vue({
   data: {
     ricercaUtente: '',
     apiKey: '75fa11ba07d05d73ee35e8943a4a42f9',
-    tipoVisibile: false,
+    tipoFilm: false,
+    tipoSerie: false,
     popolariVisibili: true,
     filmTrovati: [],
-    serieTrovate: []
+    serieTrovate: [],
+    generi: []
   },
   mounted: function () {
 
@@ -26,6 +28,22 @@ var app = new Vue({
 
     });
 
+    // richiesta generi film
+    axios.get('https://api.themoviedb.org/3/genre/movie/list?api_key=' + this.apiKey + '&language=it-IT')
+    .then(risultato => {
+
+      this.generi = this.generi.concat(risultato.data.genres);
+
+    });
+
+    // richiesta generi serie
+    axios.get('https://api.themoviedb.org/3/genre/tv/list?api_key=' + this.apiKey + '&language=it-IT')
+    .then(risultato => {
+
+      this.generi = this.generi.concat(risultato.data.genres);
+
+    });
+
   },
   methods: {
     // funzione per filtrare film/serie tv
@@ -36,7 +54,8 @@ var app = new Vue({
 
         this.filmTrovati = [];
         this.serieTrovate = [];
-        this.tipoVisibile = true;
+        this.tipoFilm = true;
+        this.tipoSerie = true;
         this.popolariVisibili = false;
 
         // richiesta film
@@ -61,16 +80,16 @@ var app = new Vue({
 
     },
     // funzione per convertire i voti
-    conversioneVoto: function (voti) {
+    conversioneVoto: function (voto) {
 
-      return Math.ceil(voti.vote_average / 2);
+      return Math.ceil(voto / 2);
 
     },
     // funzione per generare il percorso per la cover
-    generatoreCopertina: function (foto) {
+    generatoreCopertina: function (poster) {
 
-      if (foto != null) {
-        return 'https://image.tmdb.org/t/p/' + 'w342' + foto;
+      if (poster != null) {
+        return 'https://image.tmdb.org/t/p/' + 'w500' + poster;
       } else {
         return 'img/no-poster.png';
       }
@@ -80,6 +99,21 @@ var app = new Vue({
     generatoreBadiare: function (lingua) {
 
       return 'img/flags/' + lingua + '-100.png';
+
+    },
+    // funzione per generare i generi
+    generatoreGenere: function (codice) {
+
+      var nomeGenre;
+
+      // ciclo in array this.generi per asseggare ad ogni genere id il suo nome
+      this.generi.forEach((genere) => {
+        if (codice == genere.id) {
+          nomeGenre = genere.name;
+        }
+      });
+
+      return nomeGenre;
 
     }
 
